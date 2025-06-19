117094a6afbb872953c3c33f7144799be66c1730
aditya-pushkar <adityapushkar850@gmail.com>
2025-06-19 12:10:50 +0530

    groq, gemini, openrouter, chatgpt supported

--- src/components/app/ApiKeyDialog.tsx
+++ src/components/app/ApiKeyDialog.tsx
@@ -4,15 +4,77 @@ import {
   DialogHeader,
   DialogTitle,
   DialogDescription,
-  DialogFooter,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Loader as Loader, XIcon } from "lucide-react";
-import { useEffect, useState, type ChangeEvent, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 
 import { useCompletion } from "@ai-sdk/react";
 import { api } from "@/lib/baseUrl";
+import useSWR from "swr";
+import { getModels } from "@/lib/fetch";
+import type { AvailableModels } from "@/lib/types";
+
+import { saveKeys, getApiKey } from "@/lib/utils";
+
+import { LLMProviderIcons } from "@/lib/utils";
+
+
+// input API KEY, llm is llm model
+//llmname is the unique udentifier of the llm
+const ApiInput = ({ llmTitle, llmName}: { llmTitle:string, llmName: string }) => {
+  const [isVerified, setIsVerified] = useState(false);
+  const { input, handleInputChange, handleSubmit, isLoading, error, setInput } =
+    useCompletion({
+      streamProtocol: "text",
+      api: `${api}/ai/verify-key`,
+      credentials: "include",
+      body: {
+        llm: llmName,
+      },
+      onFinish: () => {
+        saveKeys(llmName, input)
+        setIsVerified(true);
+      },
+    });
+
+  useEffect(()=>{
+    const isLLMKeyStored = getApiKey(llmName)
+    if(isLLMKeyStored){
+      setInput(isLLMKeyStored)
+    }
+  }, [llmName, setInput])
+
+  return (
+    <div>
+      <div className="flex items-center py-2">
+        <div className="h-7 w-7 flex items-center justify-center mr-3">
+          <img src={LLMProviderIcons[llmName]} className="rounded-[16px]" />
+        </div>
+        <div className="flex gap-3 items-center">
+          <span className="text-sm">{llmTitle}</span>
+        </div>
+      </div>
+      <form onSubmit={handleSubmit} className="flex gap-3">
+        <Input
+          onChange={handleInputChange}
+          value={input}
+          disabled={isLoading}
+          autoFocus={false}
+          autoComplete="off"
+          minLength={10}
+          required
+          className={error && "border-destructive"}
+        />
+        <Button type="submit" disabled={isLoading || !input} className="">
+          {isLoading && <Loader className="animate-spin" />}{" "}
+          {isVerified ? "Saved!" : "Save"}
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 const APIKeysDialog = ({
   openWindow,
@@ -21,39 +83,9 @@ const APIKeysDialog = ({
   openWindow: boolean;
   windowState: (state: boolean) => void;
 }) => {
-  const [isVerified, setIsVerified] = useState(false);
-  const [localORKey, setLocalORKey] = useState<string|null>(null)
-  const { input, handleInputChange, handleSubmit, isLoading, error,setInput } =
-    useCompletion({
-      streamProtocol: "text",
-      api: `${api}/ai/verify-key`,
-      credentials: "include",
-      onFinish: ()=>{
-        setIsVerified(true)
-      },
-    });
+  const { data: llms} = useSWR<AvailableModels[]>(`${api}/ai/models`, getModels)
 
-    const saveAPIKey = () => {
-        if(!isVerified) {
-            alert("Please verify your API key first")
-            return;
-        }
-        localStorage.setItem('openrouter-key', JSON.stringify(input))
-        setLocalORKey(input)
-        setInput("")
-    }
-
-    const getKey = ()=> {
-        const hasKey = localStorage.getItem("openrouter-key")
-        if(hasKey){
-            const orKey = JSON.parse(hasKey) as string
-            setLocalORKey(orKey)
-        }
-    }
-
-    useEffect(()=>{
-        getKey()
-    },[])
+  if(!llms) return <div>Loading....</div>
 
   return (
     <Dialog open={openWindow}>
@@ -75,61 +107,11 @@ const APIKeysDialog = ({
           </DialogDescription>
         </DialogHeader>
 
-        <div className="space-y-3 mt-3">
-          <div className="flex items-center">
-            <div className="h-8 w-8 flex items-center justify-center mr-3">
-              <img src="/openrouter.jpeg" className="rounded-[16px]" />
-            </div>
-            <div className="flex gap-3 items-center">
-              <span className="text-sm">Open Router</span>
-              <a href="#" className="text-xs text-blue-400">
-                ( Get your key here )
-              </a>
-            </div>
-          </div>
-          <form
-          onSubmit={handleSubmit}
-           className="flex gap-3">
-            <Input 
-            onChange={(e:ChangeEvent<HTMLInputElement>)=>{
-                handleInputChange(e)
-                if(isVerified){
-                    setIsVerified(false)
-                }
-            }}
-            value={input}
-            disabled={isLoading}
-            autoFocus={false}
-            autoComplete="off" 
-            required
-            />
-            <Button 
-            type="submit"
-            disabled={!input}
-            className="">
-                {isLoading&&<Loader className="animate-spin"/>} Verify
-            </Button>
-          </form>
+        <div className="space-y-5 mt-3">
+          {llms.map((model) => (
+            <ApiInput llmTitle={model.title} llmName={model.name} key={model.id} />
+          ))}
         </div>
-
-        <DialogFooter>
-          <div className="w-full flex flex-col gap-3">
-            <Button
-              type="button"
-              variant={error!==undefined?"destructive":"secondary"}
-              className="px-8 mt-5 opacity-75 disabled:bg-transparent disabled:border disabled:text-foreground disabled:opacity-80"
-              disabled={!isVerified || error!==undefined || !input}
-              onClick={saveAPIKey}
-            >
-            {error!==undefined?"Invalid API Key":"Save"}
-            </Button>
-            {localORKey&&(
-                <span className="text-xs text-base-400 font-semibold">
-              Saved: <span className="tracking-tighter font-light">{localORKey}</span>
-            </span>
-            )}
-          </div>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
@@ -144,10 +126,7 @@ export const ApiKeysDialogBtn = ({ children }: { children: ReactNode }) => {
 
   return (
     <>
-      <div
-      role="button"
-        onClick={() => handleDialogWindowState(true)}
-      >
+      <div role="button" onClick={() => handleDialogWindowState(true)}>
         {children}
       </div>
       <APIKeysDialog
